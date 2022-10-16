5531c623f7a88622af7957885d757fea41ba199a
tihiro-san <ec2-user@ip-172-31-44-178.ap-northeast-1.compute.internal>
2022-10-16 04:45:52 +0000

    update失敗時のエラーメッセージ実装

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -33,14 +33,14 @@ class BooksController < ApplicationController
   def update
 
 
-    book = Book.find(params[:id])
+    @book = Book.find(params[:id])
 
-    if book.update(book_params)
+    if @book.update(book_params)
      flash[:notice]= "Book was successfully updated."
-     redirect_to  book_path(book.id)
+     redirect_to  book_path(@book.id)
     else
      @books = Book.all
-     flash.now[:error] = book.errors.full_messages
+     flash.now[:error] = @book.errors.full_messages
      render :edit
 
     end
