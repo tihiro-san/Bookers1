080f48a4545084538f9a4274c6c91bf8ef75ed91
tihiro-san <ec2-user@ip-172-31-44-178.ap-northeast-1.compute.internal>
2022-10-18 08:08:53 +0000

    maigration ファイル名編集

--- db/migrate/20221005073304_change_trashes_to_trash_objects.rb
+++
@@ -1,5 +0,0 @@
-class ChangeTrashesToTrashObjects < ActiveRecord::Migration[6.1]
-  def change
-    rename_table :lists, :books
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_10_05_073304) do
+ActiveRecord::Schema.define(version: 2022_10_05_064924) do
 
   create_table "books", force: :cascade do |t|
     t.string "title"
